cd80e669f1af0f20bd6f2fda21ad6325e458184f
João Vitor Ferreira <jvferreira.20@gmail.com>
2025-06-30 17:56:42 -0300

    Ajustando nome do jogo

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -64,7 +64,7 @@ bool Game::Initialize()
         return false;
     }
 
-    mWindow = SDL_CreateWindow("TP4: Super Hero Bros", 100, 100, mWindowWidth, mWindowHeight, 0);
+    mWindow = SDL_CreateWindow("Missão Marmita", 100, 100, mWindowWidth, mWindowHeight, 0);
     if (!mWindow)
     {
         SDL_Log("Failed to create window: %s", SDL_GetError());
